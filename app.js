f30448b4e3cf769c4f41d8e0f8ac61492e1c16bb
Byeonggon Lee <gonny952@gmail.com>
2016-12-28 11:48:05 +0900

    Working view

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-var app = {}
-app.AppView = Backbone.View.extend({
-    el: ".container",
+let app = {}
+
+app.PostPreview = Backbone.Model.extend({
+});
+
+app.PostPreviews = Backbone.Collection.extend({
+    url: "http://localhost:8080/posts"
+});
+
+app.PostPreviewView = Backbone.View.extend({
+    tagName: "div",
     initialize: function() {
-        this.render();
+        this.template = _.template($(".post-preview-template").html());
     },
     render: function() {
-        this.$el.html("<h1>Hello World</h1>");
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
     }
 });
 
-app.appView = new app.AppView();
+app.PostPreviewsView = Backbone.View.extend({
+    el: ".container",
+    initialize: function() {
+        this.listenTo(this.collection, "update", this.render);
+        this.collection.fetch();
+    },
+    render: function() {
+        alert("render!!");
+        let self = this;
+        _.each(this.collection.toArray(), function(model) {
+            self.$el.append(new app.PostPreviewView({model: model}).render().$el);
+        });
+        return this;
+    }
+});
+
+
+app.postPreviews = new app.PostPreviews();
+app.postPreviewsView = new app.PostPreviewsView({collection: app.postPreviews});
